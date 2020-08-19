ee67d12a0b1261a30f108e6f6090698a7f6326e1
ryangray2 <ryangray@udel.edu>
2020-08-19 16:59:04 -0400

    Update videos.js

--- videos.js
+++ videos.js
@@ -1,111 +1,226 @@
-const caseBreak = {
-  title: "What Is A Case Break or Break in Sports Cards? Sports Cards 101 for Beginners",
-  firstHalf: "Case ",
-  secondHalf: "Breaking",
-  cardFront: "Case Breaking",
-  link: "https://youtu.be/cTkxolwXu3g",
+// const caseBreak = {
+//   title: "What Is A Case Break or Break in Sports Cards? Sports Cards 101 for Beginners",
+//   firstHalf: "Case ",
+//   secondHalf: "Breaking",
+//   cardFront: "Case Breaking",
+//   link: "https://youtu.be/cTkxolwXu3g",
+//   topic: "101",
+//   image: "cb1.jpg"
+// }
+
+// const valueofsportscards = {
+//   title: "How Much Are My Sports Card Worth?",
+//   firstHalf: "Value ",
+//   secondHalf: "Cards",
+//   cardFront: "Value",
+//   link: "https://youtu.be/cTkxolwXu3g",
+//   topic: "101",
+//   image: "val1.jpg"
+// }
+
+// const nate = {
+//   title: "First Ever Sports Card Unboxing w/ Nate Burleson",
+//   firstHalf: "Nate ",
+//   secondHalf: "Burleson",
+//   cardFront: "Nate Burleson",
+//   link: "https://youtu.be/ROud9ZQ560c",
+//   topic: "Guest",
+//   image: "g1.jpg"
+// }
+
+// const buster = {
+//   title: "Live Card Unboxing With HoopsNation's Buster Scher",
+//   firstHalf: "Buster ",
+//   secondHalf: "Scher",
+//   cardFront: "Buster Scher",
+//   link: "https://youtu.be/pS-5yYYQKYM",
+//   topic: "Guest",
+//   image: "buster.jpg"
+// }
+
+// const makeMoney = {
+//   title: "How To Make Money On Sports Cards",
+//   firstHalf: "Make ",
+//   secondHalf: "Money",
+//   cardFront: "Make Money",
+//   link: "https://youtu.be/x4gxBanxnSs",
+//   topic: "101",
+//   image: "makeMoney.jpg"
+// }
+
+// const fanmail1 = {
+//   title: "A Fan Sent In Thousands Of Sports Cards",
+//   firstHalf: "Episode ",
+//   secondHalf: "1",
+//   cardFront: "Episode 1",
+//   link: "https://youtu.be/xIOU-B50RBQ",
+//   topic: "Fan Mail",
+//   image: "fm1.jpg"
+// }
+
+// const packWars1 = {
+//   title: "We Opened A $500 Box Of Sports Cards And Pulled An Insane Rookie!",
+//   cardFront: "Episode 1",
+//   link: "https://youtu.be/cUURYY8vjZ4",
+//   topic: "Pack Wars",
+//   image: "pw1.jpg"
+// }
+
+// const packWars2 = {
+//   title: "What Should The Punishment Be? Pack Wars: Episode 2",
+//   firstHalf: "Episode ",
+//   secondHalf: "2",
+//   cardFront: "Episode 2",
+//   link: "https://youtu.be/HAPC5sLUzro",
+//   topic: "Pack Wars",
+//   image: "pw2.jpg"
+// }
+
+// const fiveMistakes = {
+//   title: "5 HUGE Mistakes I Made Starting Out In Sports Card Investing",
+//   cardFront: "5 Huge Mistakes",
+//   link: "https://youtu.be/z6UWW3u9PtM",
+//   topic: "Invest",
+//   image: "5mistakes.jpg"
+// }
+
+// const tipsWithGary = {
+//   title: "11 Tips To Sports Card Investing in 2020",
+//   cardFront: "GaryVee Tips",
+//   link: "https://www.youtube.com/watch?v=Pjsf7ImcFDI",
+//   topic: "Invest",
+//   image: "garyVee.jpg"
+// }
+
+// const clean = {
+//   title: "How to Grade and Clean Your Sports Cards Before Submitting To PSA",
+//   cardFront: "Grade/ Clean",
+//   link: "https://www.youtube.com/watch?v=t28rOSnMcck",
+//   topic: "Clean",
+//   image: "clean.jpg"
+// }
+
+// const invest101 = {
+//   title: "Modern Sports Card Investing 101",
+//   cardFront: "Investing 101",
+//   link: "https://www.youtube.com/watch?v=cZBYfgdwaSQ",
+//   topic: "Invest",
+//   image: "invest101.jpg"
+// }
+
+
+const value101 = {
+  title: "How Do I Know the Value of my Sports Cards",
+  cardFront: "Value 101",
+  link: "https://youtu.be/oxm7t3qiLXY",
   topic: "101",
-  image: "cb1.jpg"
+  image: ""
 }
 
-const valueofsportscards = {
-  title: "How Much Are My Sports Card Worth?",
-  firstHalf: "Value ",
-  secondHalf: "Cards",
-  cardFront: "Value",
-  link: "https://youtu.be/cTkxolwXu3g",
+
+const types101 = {
+  title: "Different Types of Sports Cards",
+  cardFront: "Types",
+  link: "https://youtu.be/H48oC6jvgo4",
   topic: "101",
-  image: "val1.jpg"
+  image: ""
 }
 
-const nate = {
-  title: "First Ever Sports Card Unboxing w/ Nate Burleson",
-  firstHalf: "Nate ",
-  secondHalf: "Burleson",
-  cardFront: "Nate Burleson",
-  link: "https://youtu.be/ROud9ZQ560c",
-  topic: "Guest",
-  image: "g1.jpg"
+const pc101 = {
+  title: "Personal Collection",
+  cardFront: "Collection",
+  link: "https://youtu.be/VWxLCAj7cps",
+  topic: "101",
+  image: ""
 }
 
-const buster = {
-  title: "Live Card Unboxing With HoopsNation's Buster Scher",
-  firstHalf: "Buster ",
-  secondHalf: "Scher",
-  cardFront: "Buster Scher",
-  link: "https://youtu.be/pS-5yYYQKYM",
-  topic: "Guest",
-  image: "buster.jpg"
-}
-
-const makeMoney = {
-  title: "How To Make Money On Sports Cards",
-  firstHalf: "Make ",
-  secondHalf: "Money",
+const money101 = {
+  title: "Make Money Investing in Sports Cards",
   cardFront: "Make Money",
-  link: "https://youtu.be/x4gxBanxnSs",
+  link: "https://youtu.be/RvKhmJfsvzg",
   topic: "101",
-  image: "makeMoney.jpg"
+  image: ""
 }
 
-const fanmail1 = {
-  title: "A Fan Sent In Thousands Of Sports Cards",
-  firstHalf: "Episode ",
-  secondHalf: "1",
-  cardFront: "Episode 1",
-  link: "https://youtu.be/xIOU-B50RBQ",
-  topic: "Fan Mail",
-  image: "fm1.jpg"
+const grade101 = {
+  title: "Grading Sports Cards",
+  cardFront: "Grading",
+  link: "https://youtu.be/fX1gCHRinEA",
+  topic: "101",
+  image: ""
 }
 
-const packWars1 = {
-  title: "We Opened A $500 Box Of Sports Cards And Pulled An Insane Rookie!",
-  cardFront: "Episode 1",
-  link: "https://youtu.be/cUURYY8vjZ4",
-  topic: "Pack Wars",
-  image: "pw1.jpg"
+//////
+
+const hourWeb = {
+  title: "1 - Hour Webinar",
+  cardFront: "Hour Webinar",
+  link: "https://youtu.be/Lpx99ZU1uxk",
+  topic: "Webinars",
+  image: ""
 }
 
-const packWars2 = {
-  title: "What Should The Punishment Be? Pack Wars: Episode 2",
-  firstHalf: "Episode ",
-  secondHalf: "2",
-  cardFront: "Episode 2",
-  link: "https://youtu.be/HAPC5sLUzro",
-  topic: "Pack Wars",
-  image: "pw2.jpg"
+const fanWeb = {
+  title: "Answering Fan Questions",
+  cardFront: "Fan Q's",
+  link: "https://www.youtube.com/watch?v=hzniryVgRkg",
+  topic: "Webinars",
+  image: ""
 }
 
-const fiveMistakes = {
-  title: "5 HUGE Mistakes I Made Starting Out In Sports Card Investing",
-  cardFront: "5 Huge Mistakes",
-  link: "https://youtu.be/z6UWW3u9PtM",
-  topic: "Invest",
-  image: "5mistakes.jpg"
+///////////
+
+const allGary = {
+  title: "Everything You Need To Know",
+  cardFront: "All-In-One",
+  link: "https://www.youtube.com/watch?v=TU_jtMR_ANA",
+  topic: "Gary Vee",
+  image: ""
 }
 
-const tipsWithGary = {
-  title: "11 Tips To Sports Card Investing in 2020",
-  cardFront: "GaryVee Tips",
+const elevenGary = {
+  title: "11 Tips to Invest",
+  cardFront: "Investing",
   link: "https://www.youtube.com/watch?v=Pjsf7ImcFDI",
-  topic: "Invest",
-  image: "garyVee.jpg"
+  topic: "Gary Vee",
+  image: ""
 }
 
-const clean = {
-  title: "How to Grade and Clean Your Sports Cards Before Submitting To PSA",
-  cardFront: "Grade/ Clean",
-  link: "https://www.youtube.com/watch?v=t28rOSnMcck",
-  topic: "Clean",
-  image: "clean.jpg"
+const breakfastGary = {
+  title: "Breakfast Club",
+  cardFront: "Breakfast",
+  link: "https://www.youtube.com/watch?v=0iogCj_ttYs",
+  topic: "Gary Vee",
+  image: ""
 }
 
-const invest101 = {
-  title: "Modern Sports Card Investing 101",
-  cardFront: "Investing 101",
-  link: "https://www.youtube.com/watch?v=cZBYfgdwaSQ",
-  topic: "Invest",
-  image: "invest101.jpg"
+const funGary = {
+  title: "Fun Moments",
+  cardFront: "Fun",
+  link: "https://www.youtube.com/watch?v=xk6qdH78Rg4",
+  topic: "Gary Vee",
+  image: ""
 }
 
-const videoArray = [caseBreak, nate, buster, makeMoney, fanmail1, fiveMistakes, packWars1, packWars2, tipsWithGary, clean, invest101]
+const busterFun = {
+  title: "Buster x Jack Settleman",
+  cardFront: "Buster/Jack",
+  link: "https://www.youtube.com/watch?v=eqSeyHZ0bbw",
+  topic: "Fun Content",
+  image: ""
+}
+
+const fourFun = {
+  title: "$400,000 Sports Cards",
+  cardFront: "$400,000",
+  link: "https://www.youtube.com/watch?v=BLZDV5zc2Vg",
+  topic: "Fun Content",
+  image: ""
+}
+
+
+
+
+
+// const videoArray = [caseBreak, nate, buster, makeMoney, fanmail1, fiveMistakes, packWars1, packWars2, tipsWithGary, clean, invest101]
+const videoArray = [value101, types101, pc101, money101, grade101, hourWeb, fanWeb, allGary, elevenGary, breakfastGary, funGary, busterFun, fourFun]
